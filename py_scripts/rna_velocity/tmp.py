b1b09e4d33cb616680410ad26cc2127be8c4521c
Akanksha Sachan <asachan@r193.ib.bridges2.psc.edu>
2024-08-01 23:27:12 -0400

    ranked genes in velocity

--- py_scripts/rna_velocity/tmp.py
+++ py_scripts/rna_velocity/tmp.py
@@ -26,7 +26,7 @@ def main(args):
         ldata_list = pool.map(read_loom_file, loom_files)
 
     # Rename barcodes in order to merge
-    suffixes = ['_7', '_15', '_4', '_11']
+    suffixes = ['_15', '_11']
     for ldata, suffix in zip(ldata_list, suffixes):
         barcodes = [bc.split(':')[1] for bc in ldata.obs.index.tolist()]
         barcodes = [bc[0:len(bc)-1] + suffix for bc in barcodes]
@@ -36,15 +36,16 @@ def main(args):
     # Concatenate the looms
     ldata = ldata_list[0].concatenate(ldata_list[1:])
 
-    # Merge matrices into the original adata object
+    # Merge matrices into the original adata object {the order of cells for merging matters > for matching?}
     adata_with_vcy = scv.utils.merge(adata_rna, ldata)
 
     # Save the new adata object with loom data
     adata_with_vcy.write(args.output)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="get velocities")
+    parser = argparse.ArgumentParser(description="get loom plus anndata")
     parser.add_argument("--anndata", type=str, required=True, help="Path to the anndata file.")
+    parser.add_argument("--loom_files", type=str, nargs='+', required=True, help="Paths to the loom files.")
     parser.add_argument("--output", type=str, required=True, help="Path to the output file.")
     
     args = parser.parse_args()
